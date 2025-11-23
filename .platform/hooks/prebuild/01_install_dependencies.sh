e4984b64feeb4e194ede321d611ea91794fe5d96
jeffersonmojica <jeffermojika@gmail.com>
2025-11-22 23:58:10 -0500

    correciones

--- .platform/hooks/prebuild/01_install_dependencies.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd /var/app/staging/backend
-pip install -r requirements.txt
--- application.py
+++ application.py
@@ -1,23 +1,3 @@
-#!/usr/bin/env python
 import sys
-import os
-
-# Get current directory
-current_dir = os.path.dirname(os.path.abspath(__file__))
-backend_dir = os.path.join(current_dir, 'backend')
-
-# Add to path
-sys.path.insert(0, current_dir)
-sys.path.insert(0, backend_dir)
-
-# Change to backend directory
-try:
-    os.chdir(backend_dir)
-except:
-    pass
-
-# Import Flask app
+sys.path.insert(0, 'backend')
 from app import app as application
-
-if __name__ == '__main__':
-    application.run(host='0.0.0.0', port=8080)
